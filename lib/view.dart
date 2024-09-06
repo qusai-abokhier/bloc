943ea56cb7f5392f78d7b460cd55be9a501a0e02
qusai <qusai2000abokhier@gmail.com>
2024-09-06 21:07:49 +0300

    add view

---
+++ lib/view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class View extends StatelessWidget {
+  const View({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container() ;
+  }
+  }
